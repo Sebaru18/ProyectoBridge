5b7808872789d374a46df399cf95688c0cd93050
DavidSArbelaez <108475569+DavidSArbelaez@users.noreply.github.com>
2023-04-21 21:34:43 +0000

    Agrego solucion error

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,7 +2,7 @@
 public class Launcher {
 public static void main(String[] args) {
 // Crear la pregunta
-Pregunta preguntaNumerica = new PreguntaNumerica("¿Qué tan útil te resultó esta herramienta? (1-5)");
+Pregunta preguntaNumerica = new PreguntaNumerica("¿Qué tan bueno te resultó esta app? (1-5)");
 //Pregunta preguntaSiNo = new PreguntaSiNo("¿Recomendarías esta herramienta a un amigo? (Sí/No)");
     // Crear la encuesta y hacerla
     Encuesta encuesta = new EncuestaBasica(preguntaNumerica);
