9db0f9411d6c97ffe42d4655ccd3109d6c4a553b
juankchb <89666771+juankchb@users.noreply.github.com>
2023-05-05 02:10:16 +0000

    update

---
+++ src/Informe.java
@@ -0,0 +1,29 @@
+public class Informe{
+    public Encuesta encuesta;
+
+    public Encuesta(){
+        
+    }
+
+
+    public void promedioTipo(){
+
+    }
+
+    public void cantidadRespuestas(){
+
+    }
+
+    public void cantidadRespuestasTipo(){
+
+    }
+
+    public void hacerPreguntaAleatoria(int cantidadPreguntas){
+
+    }
+
+    public void hacerPreguntaTipo(int cant, String tipo){
+
+    }
+
+}
