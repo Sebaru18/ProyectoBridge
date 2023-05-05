75688696787acf1103eb1d3ecfe7b17b52e0a9be
Sebaru18 <89612368+Sebaru18@users.noreply.github.com>
2023-05-05 15:57:56 -0500

    Create Interface

---
+++ src/Informe1/InformeInterface.java
@@ -0,0 +1,7 @@
+public interface InformeInterface (){
+  
+  public void promediotipo();
+  public int cantidadRespuestas();
+  public void cantidadRespuestasTipo();
+  public void definirPregunta(int tipoP);  
+}
